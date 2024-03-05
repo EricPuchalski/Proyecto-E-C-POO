17897cdf5e45fcd3705bf4bd55387f16b3e7eb87
facundo pedrozo <facundopedrozo2@gmail.com>
2024-03-05 20:27:21 -0300

    Update OrderServiceTest.java

--- proyecto-final-poo/src/test/java/OrderServiceTest.java
+++ proyecto-final-poo/src/test/java/OrderServiceTest.java
@@ -273,29 +273,4 @@ public class OrderServiceTest {
 
 }
 
-/*
-    @Test
-    public void testCreateOrder_Success() {
-        // Arrange
-        Order order = new Order(); // Crear una instancia de Order con los datos necesarios
-        Warehouse warehouseOrig = new Warehouse(); // Instanciar Warehouse
-        Warehouse warehouseDest = new Warehouse(); // Instanciar Warehouse
-        String cuitCustomer = "123456789"; // Ejemplo de un número de CUIT válido
-        String cuitCarrier = "987654321"; // Ejemplo de un número de CUIT válido
 
-        // Configuración del mock para el método findOrderByOrderNumber
-        when(orderRepositoryMock.findOrderByOrderNumber(anyString())).thenReturn(null);
-
-        // Configuración del mock para el método createOrder
-        when(orderRepositoryMock.createOrder(any(Order.class), any(Warehouse.class), any(Warehouse.class), anyString(), anyString())).thenReturn(order);
-
-        // Act
-        Order createdOrder = orderService.createOrder(order, warehouseOrig, warehouseDest, cuitCustomer, cuitCarrier);
-
-        // Assert
-        assertNotNull(createdOrder); // Verificar que la orden creada no sea nula
-        // Aquí puedes agregar más aserciones si hay otros datos que quieres verificar en la orden creada
-        verify(orderRepositoryMock, times(1)).findOrderByOrderNumber(anyString()); // Verificar que se llamó al método findOrderByOrderNumber una vez
-        verify(orderRepositoryMock, times(1)).createOrder(any(Order.class), any(Warehouse.class), any(Warehouse.class), anyString(), anyString()); // Verificar que se llamó al método createOrder una vez
-    }
- */
