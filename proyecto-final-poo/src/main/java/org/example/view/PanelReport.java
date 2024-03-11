5fccef31af6a00f022ed173785da59c1e159fbbd
Eric Puchalski <ericpuchalski12@gmail.com>
2024-03-11 16:41:13 -0300

    update
    arreglando eliminar proveedor para que los productos tambien se deshabiliten y agregue un metodo para ver las lineas de pedidos

--- proyecto-final-poo/src/main/java/org/example/view/PanelReport.java
+++ proyecto-final-poo/src/main/java/org/example/view/PanelReport.java
@@ -31,9 +31,12 @@ public class PanelReport extends javax.swing.JPanel {
 
         lblPresent = new javax.swing.JLabel();
         bttnComplement = new javax.swing.JLabel();
-        bttnCreateCustomer = new javax.swing.JButton();
+        bttnOrderLines = new javax.swing.JButton();
         bttnConsultCustomer = new javax.swing.JButton();
         bttnBack = new javax.swing.JButton();
+        bttnCreateCustomer1 = new javax.swing.JButton();
+
+        setBackground(new java.awt.Color(102, 153, 255));
 
         lblPresent.setFont(new java.awt.Font("Roboto Black", 1, 48)); // NOI18N
         lblPresent.setForeground(new java.awt.Color(232, 245, 255));
@@ -43,14 +46,14 @@ public class PanelReport extends javax.swing.JPanel {
         bttnComplement.setForeground(new java.awt.Color(232, 245, 255));
         bttnComplement.setText("Por favor seleccione la opcion que desee");
 
-        bttnCreateCustomer.setBackground(new java.awt.Color(0, 0, 102));
-        bttnCreateCustomer.setFont(new java.awt.Font("Roboto", 1, 18)); // NOI18N
-        bttnCreateCustomer.setForeground(new java.awt.Color(0, 195, 225));
-        bttnCreateCustomer.setText("VER PEDIDOS DE UN CLIENTE");
-        bttnCreateCustomer.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        bttnCreateCustomer.addActionListener(new java.awt.event.ActionListener() {
+        bttnOrderLines.setBackground(new java.awt.Color(0, 0, 102));
+        bttnOrderLines.setFont(new java.awt.Font("Roboto", 1, 18)); // NOI18N
+        bttnOrderLines.setForeground(new java.awt.Color(0, 195, 225));
+        bttnOrderLines.setText("VER LINEAS DE PEDIDO");
+        bttnOrderLines.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        bttnOrderLines.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bttnCreateCustomerActionPerformed(evt);
+                bttnOrderLinesActionPerformed(evt);
             }
         });
 
@@ -76,6 +79,17 @@ public class PanelReport extends javax.swing.JPanel {
             }
         });
 
+        bttnCreateCustomer1.setBackground(new java.awt.Color(0, 0, 102));
+        bttnCreateCustomer1.setFont(new java.awt.Font("Roboto", 1, 18)); // NOI18N
+        bttnCreateCustomer1.setForeground(new java.awt.Color(0, 195, 225));
+        bttnCreateCustomer1.setText("VER PEDIDOS DE UN CLIENTE");
+        bttnCreateCustomer1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        bttnCreateCustomer1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bttnCreateCustomer1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -89,9 +103,10 @@ public class PanelReport extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGap(204, 204, 204)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(bttnOrderLines, javax.swing.GroupLayout.PREFERRED_SIZE, 333, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bttnCreateCustomer1, javax.swing.GroupLayout.PREFERRED_SIZE, 333, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(bttnBack, javax.swing.GroupLayout.PREFERRED_SIZE, 333, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(bttnConsultCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, 333, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(bttnCreateCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, 333, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(bttnConsultCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, 333, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -101,19 +116,21 @@ public class PanelReport extends javax.swing.JPanel {
                 .addComponent(lblPresent)
                 .addGap(87, 87, 87)
                 .addComponent(bttnComplement)
-                .addGap(43, 43, 43)
-                .addComponent(bttnCreateCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(44, 44, 44)
+                .addGap(52, 52, 52)
+                .addComponent(bttnCreateCustomer1, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(bttnConsultCustomer, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(38, 38, 38)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(bttnOrderLines, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(bttnBack, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(165, Short.MAX_VALUE))
+                .addContainerGap(135, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void bttnCreateCustomerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bttnCreateCustomerActionPerformed
-        ViewController.panelChange(this, new PanelReportOrdersByCustomer(), this);
-    }//GEN-LAST:event_bttnCreateCustomerActionPerformed
+    private void bttnOrderLinesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bttnOrderLinesActionPerformed
+        ViewController.panelChange(this, new PanelSeeOrderLines(), this);
+    }//GEN-LAST:event_bttnOrderLinesActionPerformed
 
     private void bttnConsultCustomerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bttnConsultCustomerActionPerformed
         ViewController.panelChange(this, new PanelReportOrdersByWarehouseOrig(), this);
@@ -123,12 +140,17 @@ public class PanelReport extends javax.swing.JPanel {
         ViewController.panelChange(this, new PanelMain(), this);
     }//GEN-LAST:event_bttnBackActionPerformed
 
+    private void bttnCreateCustomer1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bttnCreateCustomer1ActionPerformed
+        ViewController.panelChange(this, new PanelReportOrdersByCustomer(), this);
+    }//GEN-LAST:event_bttnCreateCustomer1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bttnBack;
     private javax.swing.JLabel bttnComplement;
     private javax.swing.JButton bttnConsultCustomer;
-    private javax.swing.JButton bttnCreateCustomer;
+    private javax.swing.JButton bttnCreateCustomer1;
+    private javax.swing.JButton bttnOrderLines;
     private javax.swing.JLabel lblPresent;
     // End of variables declaration//GEN-END:variables
 }
